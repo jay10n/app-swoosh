316a024f2b3027d5b5db206a5950bbf39717b410
Jayton Schmeeckle <jvschmeeckle@gmail.com>
2017-12-31 14:24:14 -0600

    added constraints for auto layout

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var bgImg: UIImageView!
+    @IBOutlet weak var swoosh: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    
 }
 
