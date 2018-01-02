6b347b93ccd067c628cfabd421ef360b0aff1f59
Jayton Schmeeckle <jvschmeeckle@gmail.com>
2018-01-02 13:09:49 -0600

    added player model to store league and connected btns to VC

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -10,10 +10,13 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    var player: Player!
+    @IBOutlet weak var nextBtn: BorderButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        player = Player()
         // Do any additional setup after loading the view.
     }
 
@@ -21,8 +24,23 @@ class LeagueVC: UIViewController {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
     
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
     
-
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+    
     /*
     // MARK: - Navigation
 
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Jayton Schmeeckle on 1/2/18.
+//  Copyright © 2018 Jayton Schmeeckle. All rights reserved.
+//
+
+import Foundation
+
+// Stores values for player types
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
